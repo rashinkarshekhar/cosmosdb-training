b2a90aa81a7a6919a69b24a0902e6ba67110041a
rashinkarshekhar <125436510+rashinkarshekhar@users.noreply.github.com>
2023-02-15 07:30:18 +0000

    sir

--- python.py
+++ python.py
@@ -1,19 +1,23 @@
-
 # libraries
 import getpass
 import pymongo
 from random import randint
+# Connection string to connect with CosmosDB MongoDB API
 CONNECTION_STRING = getpass.getpass(
     prompt="Enter connection string available in networking tab of cosmos account: "
 )
 #print("Using " + CONNECTION_STRING + "to connect with MongoDB")
-
-# variables
+# Variables for Database & collection to create, update or delete the documents
 DATABASE = "thinknyx"
 COLLECTION = "thinknyx"
 FIELD = "Day"
-
-
+def insert_sample_document(collection):
+    """Insert a sample document and return the contents of its _id field"""
+    document_id = collection.insert_one(
+        {FIELD: randint(50, 500)}
+    ).inserted_id
+    print("Inserted document with _id {}".format(document_id))
+    return document_id
 def create_database_unsharded_collection(client):
     """Create sample database with shared throughput if it doesn't exist and
     an unsharded collection
@@ -24,7 +28,7 @@ def create_database_unsharded_collection(client):
         # Database with 400 RU throughput that can be shared across the
         # DB's collections
         db.command({"customAction": "CreateDatabase", "offerThroughput": 400})
-        print("Created db {} with shared throughput".format DATABASE)
+        print("Created db {} with shared throughput".format(DATABASE))
     # Create collection if it doesn't exist
     if COLLECTION not in db.list_collection_names():
         # Creates a unsharded collection that uses the DBs shared throughput
@@ -34,9 +38,8 @@ def create_database_unsharded_collection(client):
                 "collection": COLLECTION,
             }
         )
-        print("Created collection {}".format COLLECTION)
-    return db COLLECTION
-
+        print("Created collection {}".format(COLLECTION))
+    return db[COLLECTION]
 def main():
     """Connect to the API for MongoDB, create DB and collection, perform
     CRUD operations
@@ -50,11 +53,6 @@ def main():
                 or timed out when attempting to connect"
         )
     collection = create_database_unsharded_collection(client)
-    
+    document_id = insert_sample_document(collection)
 if __name__ == "__main__":
     main()
-
-    def insert_sample_document(collection):
-    """Insert a sample document and return the contents of its _id field"""
-    document_id = collection.insert_one(
-        {FIELD: randint(50, 500)}
